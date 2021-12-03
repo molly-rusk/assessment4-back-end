c557fe9d1b6a2aa92443acb2f0c067498d5af15b
Molly Rusk <mollyrusk@gmail.com>
2021-12-03 12:08:25 -0700

    finished assessment

--- server/index.js
+++ server/index.js
@@ -22,4 +22,70 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+// generate random fortune
+app.get("/api/fortune/", (req, res) => {
+
+  const fortunes = ["You will receive good news today!",
+                    "You will become great if you believe in yourself",
+                    "Now is the time to try something new",
+                    "Wealth awaits you very soon",
+                    "Your ability for accomplishment will follow with success"]
+
+  let fortuneIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[fortuneIndex];
+
+  res.status(200).send(randomFortune)
+
+
+})
+
+// generate a random reminder
+
+app.get("/api/reminder/", (req, res) => {
+
+  const reminders = ["Water your plants",
+                     "Don't skip breakfast",
+                     "Check the mail",
+                     "Feed the animals"]
+
+  let reminderIndex = Math.floor(Math.random() * reminders.length);
+  let randomReminder = reminders[reminderIndex];
+
+  res.status(200).send(randomReminder)
+
+
+})
+
+// generate a random quote
+
+app.get("/api/quote/", (req, res) => {
+
+  const quotes = ["Life is 10% of what happens to you and 90% how you react to it. -Charles R. Swindoll",
+                  "Aim for the moon. If you miss, you may hit a star. -W. Clement Stone",
+                  "Success is a journey not a destination. -Ben Sweetland"]
+
+  let quoteIndex = Math.floor(Math.random() * quotes.length);
+  let randomQuote = quotes[quoteIndex];
+
+  res.status(200).send(randomQuote)
+
+
+})
+
+// generate a random emotion
+
+app.get("/api/emoticon/", (req, res) => {
+
+  const emoticons = [":)", ":D", ":O", ":P"]
+
+  let emoticonIndex = Math.floor(Math.random() * emoticons.length);
+  let randomEmoticon = emoticons[emoticonIndex];
+
+  res.status(200).send(randomEmoticon)
+
+
+})
+
+
+
 app.listen(4000, () => console.log("Server running on 4000"));
